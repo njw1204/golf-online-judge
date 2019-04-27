26394abdc8a0abd010cbc5354c661239980928f4
Jongwoo Na <njw1204@naver.com>
2019-04-27 14:34:14 +0900

    Basic setup

--- goj/goj/settings.py
+++ goj/goj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1kp-u=t8$!k-*f1b4*wedbmv1_yyc#)yku(=6y^u=cu6d61cia'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainApp',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 

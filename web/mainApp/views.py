fe34824368f715fbd8a333021663659bba4089b1
Jongwoo Na <njw1204@naver.com>
2019-04-27 17:54:52 +0900

    Make login session when signup

--- web/mainApp/views.py
+++ web/mainApp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 from mainApp.forms import CustomUserCreateForm
 
 # Create your views here.
@@ -9,6 +10,12 @@ class SignupView(CreateView):
     template_name = "registration/signup.html"
     form_class = CustomUserCreateForm
     success_url = reverse_lazy("mainApp:index")
+
+    def form_valid(self, form):
+        self.object = form.save()
+        login(self.request, self.object)
+        return redirect(self.get_success_url())
+
 
 class IndexView(TemplateView):
     template_name = "mainApp/index.html"

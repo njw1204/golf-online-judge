d62770f6bb5cd5720f7559233969186560061da8
Jongwoo Na <njw1204@naver.com>
2019-04-29 23:59:39 +0900

    Add crispy module

--- web/goj/settings.py
+++ web/goj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'mainApp',
 ]
 
@@ -127,3 +128,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, ".static-bundle")
 LOGIN_REDIRECT_URL = 'mainApp:index'
 LOGOUT_REDIRECT_URL = 'mainApp:index'
 LOGIN_URL = 'login'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

6549dd25da6cf02bd09d4924de0e34013651f21b
Jongwoo Na <njw1204@naver.com>
2019-05-03 00:37:58 +0900

    Modify base.html & related things design

--- web/goj/settings.py
+++ web/goj/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "mainApp/static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, ".static-bundle")
 
